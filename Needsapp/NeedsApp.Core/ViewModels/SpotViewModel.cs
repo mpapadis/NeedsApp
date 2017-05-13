072e7cf0915cbdb584fac6fe86432c50b683d69b
mpapadis <mpapadis@yahoo.gr>
2017-05-13 12:59:10 +0300

    added LocationService in  SpotViewModel

--- Needsapp/NeedsApp.Core/ViewModels/SpotViewModel.cs
+++ Needsapp/NeedsApp.Core/ViewModels/SpotViewModel.cs
@@ -1,19 +1,99 @@
-﻿using NeedsApp.Core.Model;
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using NeedsApp.Core.Model;
+using NeedsApp.Core.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace NeedsApp.Core.ViewModels
 {
     [PropertyChanged.ImplementPropertyChanged]
     public class SpotViewModel:BaseViewModel
     {
+
+        private ILocationService _locationService;
+        public ILocationService LocationService {
+            get { return _locationService; }
+            set { _locationService = value; }
+        }
+
         private Spot _spot;
 
         public Spot Spot { get => _spot; set => _spot = value; }
+
+        public SpotViewModel(ILocationService locationService)
+        {
+            LocationService = locationService;
+            LoadDataCommand.Execute(null);
+        }
+
+
+        public ICommand GetLocationCommand {
+            get {
+                return new MvxAsyncCommand(DoSetLocationAsync);
+            }
+        }
+
+        //public IPermissions Permissions { get; private set; }
+
+        private int locationSearchTimeOut = 10000;
+        private CancellationTokenSource CancellationTokenSource = null;
+
+        private async Task DoSetLocationAsync()
+        {
+            if (LocationService != null)
+            {
+
+
+                Exception _ex = null;
+                IsBusy = true;
+                try
+                {
+                    //var cts = new CancellationTokenSource(locationSearchTimeOut);
+                    CancellationTokenSource = new CancellationTokenSource(locationSearchTimeOut);
+                    var t = CancellationTokenSource.Token;
+
+                    var location = await LocationService.GetCurrentLocationTask(t);
+
+                    IsBusy = false;
+                    ApplyLocation(location);
+                }
+                catch (Exception ex)
+                {
+                    _ex = ex;
+                    Mvx.Error(String.Format("Σφάλμα κατά τη λήψη θέσης GPS:{0}", ex.Message));
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
+                if (_ex != null)
+                {
+                    try
+                    {
+                        var page = new ContentPage();
+                        var result = page.DisplayAlert("Προσοχή", string.Format("Σφάλμα κατά τη λήψη θέσης GPS.{0}{1}", Environment.NewLine, _ex.Message), "OK");
+                    }
+                    catch (Exception displayalertex)
+                    {
+                        if (displayalertex != null && !String.IsNullOrEmpty(displayalertex.Message))
+                            Mvx.Error(displayalertex.Message);
+                    }
+                }
+
+            }
+        }
+
+        private void ApplyLocation(Position location)
+        {
+            SpotLocation = location;
+        }
 
         public String SpotName {
             get {
@@ -21,9 +101,13 @@ namespace NeedsApp.Core.ViewModels
             }
         }
 
+        [PropertyChanged.AlsoNotifyFor(nameof(SpotLongitude), nameof(SpotLatitude))]
         public Position SpotLocation {
             get {
                 return Spot?.SpotLocation;
+            }
+            set {
+                Spot.SpotLocation = value;
             }
         }
 
@@ -41,7 +125,35 @@ namespace NeedsApp.Core.ViewModels
         }
 
 
-        public ICommand LoadDataCommand { get { return null; } }
+        public ICommand LoadDataCommand { get
+                {
+                return new MvxAsyncCommand(LoadDataAsync);
+            }; } 
+
+        private async Task LoadDataAsync()
+        {
+            IsBusy = true;
+            try
+            {
+                //if (id == 0)
+                //{
+                    Spot = await Task.Run(() => new Spot() { ID = 10, Name = "arduino1", SpotLocation = null });
+                //}
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+           
+
+        }
+
 
         public ICommand SetLocationCommand { get { return null; } }
 
