2dda459d9fdd00ed1bed4a73c3c19947e45b4c21
mpapadis <mpapadis@yahoo.gr>
2017-05-13 12:08:39 +0300

    Added Model.Spot Added SpotViewModel

---
+++ Needsapp/NeedsApp.Core/Model/Spot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedsApp.Core.Model
+{
+    [PropertyChanged.ImplementPropertyChanged]
+    public class Spot: BaseModel
+    {
+
+        public string Name { get; set; }
+        public Position SpotLocation { get; set; }
+
+    }
+}
---
+++ Needsapp/NeedsApp.Core/ViewModels/SpotViewModel.cs
@@ -0,0 +1,55 @@
+﻿using NeedsApp.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NeedsApp.Core.ViewModels
+{
+    [PropertyChanged.ImplementPropertyChanged]
+    public class SpotViewModel:BaseViewModel
+    {
+        private Spot _spot;
+
+        public Spot Spot { get => _spot; set => _spot = value; }
+
+        public String SpotName {
+            get {
+                return Spot?.Name ?? string.Empty;
+            }
+        }
+
+        public Position SpotLocation {
+            get {
+                return Spot?.SpotLocation;
+            }
+        }
+
+        
+        public string SpotLongitude {
+            get {
+                return Spot?.SpotLocation?.Longitude?.ToString() ?? string.Empty;
+            }
+        }
+
+        public string SpotLatitude {
+            get {
+                return Spot?.SpotLocation?.Latitude?.ToString() ?? string.Empty;
+            }
+        }
+
+
+        public ICommand LoadDataCommand { get { return null; } }
+
+        public ICommand SetLocationCommand { get { return null; } }
+
+        public ICommand SaveDataCommand { get { return null; } }
+
+        public ICommand StartWatering { get { return null; } }
+
+        public ICommand StopWatering { get { return null; } }
+
+    }
+}
